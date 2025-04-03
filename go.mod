842cbef565e69a8e423e0cc7b575248f9ea65d4f
s19835 <s19835@sci.pdn.ac.lk>
2025-04-03 19:28:26 +0530

    add: testing for analyzer, bottlenecks

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/jackc/pgx/v5 v5.7.4
 
 require (
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -15,6 +16,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
@@ -22,6 +24,7 @@ require (
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/spf13/viper v1.20.1 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
---
+++ internal/analyzer/analyzer_test.go
@@ -0,0 +1,139 @@
+package analyzer_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/s19835/pg-opt-toolkit/internal/analyzer"
+	"github.com/s19835/pg-opt-toolkit/pkg/queryplan"
+	"github.com/stretchr/testify/assert"
+)
+
+const complexPlanJSON = `{
+	"Plan": {
+		"Node Type": "Limit",
+		"Startup Cost": 1024.58,
+		"Total Cost": 1026.12,
+		"Plan Rows": 100,
+		"Plan Width": 128,
+		"Actual Startup Time": 1254.32,
+		"Actual Total Time": 1256.78,
+		"Actual Rows": 100,
+		"Actual Loops": 1,
+		"Plans": [
+			{
+				"Node Type": "Sort",
+				"Startup Cost": 1024.58,
+				"Total Cost": 1030.45,
+				"Plan Rows": 2345,
+				"Plan Width": 128,
+				"Actual Startup Time": 1254.30,
+				"Actual Total Time": 1255.12,
+				"Actual Rows": 2345,
+				"Actual Loops": 1,
+				"Sort Key": ["users.created_at DESC"],
+				"Plans": [
+					{
+						"Node Type": "Hash Join",
+						"Startup Cost": 25.88,
+						"Total Cost": 924.58,
+						"Plan Rows": 2345,
+						"Plan Width": 128,
+						"Actual Startup Time": 45.32,
+						"Actual Total Time": 123.45,
+						"Actual Rows": 10000,
+						"Actual Loops": 1,
+						"Hash Cond": "(users.id = orders.user_id)",
+						"Plans": [
+							{
+								"Node Type": "Seq Scan",
+								"Parent Relationship": "Outer",
+								"Relation Name": "users",
+								"Alias": "users",
+								"Startup Cost": 0.00,
+								"Total Cost": 125.88,
+								"Plan Rows": 10000,
+								"Plan Width": 64,
+								"Actual Startup Time": 5.32,
+								"Actual Total Time": 25.45,
+								"Actual Rows": 10000,
+								"Actual Loops": 1,
+								"Filter": "(status = 'active')"
+							},
+							{
+								"Node Type": "Hash",
+								"Parent Relationship": "Inner",
+								"Startup Cost": 15.88,
+								"Total Cost": 15.88,
+								"Plan Rows": 500,
+								"Plan Width": 64,
+								"Actual Startup Time": 15.32,
+								"Actual Total Time": 15.32,
+								"Actual Rows": 500,
+								"Actual Loops": 1,
+								"Plans": [
+									{
+										"Node Type": "Seq Scan",
+										"Relation Name": "orders",
+										"Alias": "orders",
+										"Startup Cost": 0.00,
+										"Total Cost": 15.88,
+										"Plan Rows": 500,
+										"Plan Width": 64,
+										"Actual Startup Time": 0.32,
+										"Actual Total Time": 5.45,
+										"Actual Rows": 500,
+										"Actual Loops": 1,
+										"Filter": "(created_at > (now() - '30 days'::interval))"
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		]
+	},
+	"Planning Time": 12.34,
+	"Execution Time": 1268.12
+}`
+
+func TestPerformanceMatrixCalculation(t *testing.T) {
+	var plan queryplan.QueryPlan
+	err := json.Unmarshal([]byte(complexPlanJSON), &plan)
+	assert.NoError(t, err)
+
+	analyzer := analyzer.NewQueryAnalyzer()
+	result, err := analyzer.Analyze(&plan)
+	assert.NoError(t, err)
+
+	// Test latency measurement
+	assert.Contains(t, result, "Execution Time: 1268.12 ms")
+	assert.Contains(t, result, "Planning Time: 12.34 ms")
+
+	// Test const measurement
+	assert.Contains(t, result, "Cost: 1024.58..1026.12")
+	assert.Contains(t, result, "Cost: 25.88..924.58")
+}
+
+func TestBottlenecksDetection(t *testing.T) {
+	var plan queryplan.QueryPlan
+	err := json.Unmarshal([]byte(complexPlanJSON), &plan)
+	assert.NoError(t, err)
+
+	analyzer := analyzer.NewQueryAnalyzer()
+	bottlenecks := analyzer.IdentifyBottlenecks(&plan)
+
+	expectedBottlenecks := []string{
+		"Slow operation: Sort (1255.12 ms)",
+		"Slow operation: Hash Join (123.45 ms)",
+		"Inefficient operation: Seq Scan on users (25.45 ms for 10000 rows)",
+		"Potential optimization: Filter on status could use index",
+		"Potential optimization: Filter on created_at could use index",
+	}
+
+	assert.Len(t, bottlenecks, len(expectedBottlenecks))
+	for _, expected := range expectedBottlenecks {
+		assert.Contains(t, bottlenecks, expected)
+	}
+}
